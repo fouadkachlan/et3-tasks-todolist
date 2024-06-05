7bc9090f384dc8d694e2e54828764823b7df8fcd
fouadkachlan <fouad.kachlan@et3arraf.com>
2024-06-05 12:15:30 +0300

    Update Button.jsx

--- Button/Button.jsx
+++ Button/Button.jsx
@@ -3,7 +3,12 @@ import React from "react"
 
 const Button = ({onClick, btnStyle, children}) => {
     return (
-        <button style={btnStyle} onClick={onClick}>
+        <button style={{
+            padding:"15px",
+            border: "20px",
+            borderRadius: "50px 50px",
+            backgroundColor:"chartreuse" 
+        }} onClick={onClick}>
             {children}
 
         </button>
