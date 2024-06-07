b219fb58daf26639ad8a5222fc8ad4c95f720371
fouadkachlan <fouad.kachlan@et3arraf.com>
2024-06-07 11:18:35 +0300

    Update CustomButton.tsx
    Done

--- Components/CustomButton/CustomButton.tsx
+++ Components/CustomButton/CustomButton.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 interface CustomButtonProps {
   onClick: () => void;
   children: React.ReactNode;
-  style?: React.CSSProperties;
+  style: React.CSSProperties;
 }
 
 const CustomButton: React.FC<CustomButtonProps> = ({ onClick, children, style }) => {
