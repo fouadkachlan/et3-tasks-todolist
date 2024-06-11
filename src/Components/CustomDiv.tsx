806491ae04eb0cdf09912a26d344b2adf4173d59
fouadkachlan <fouad.kachlan@et3arraf.com>
2024-06-11 12:42:52 +0300

    Create CustomDiv.tsx

---
+++ src/Components/CustomDiv.tsx
@@ -0,0 +1,25 @@
+import React, { CSSProperties } from 'react';
+
+interface CustomeDivProps {
+  children: React.ReactNode;
+  style : React.CSSProperties;
+  width: string;
+  height: string;
+  display: "flex" | "block",
+  alignItems: "center" | "flex-start" | "flex-end",
+  justifyContent: "center" | "flex-start" | "flex-end" | "space-between"
+}
+const CustomDiv: React.FC<CustomeDivProps> = ({ children, style , width , height , display , alignItems , justifyContent  }) => {
+  const basicStyles : CSSProperties = {
+    width,
+    height,
+    display,
+    alignItems,
+    justifyContent
+  };
+  const finalizedStyles = {...basicStyles , style};
+  
+  return <div style={finalizedStyles}>{children}</div>;
+}
+
+export default CustomDiv;
