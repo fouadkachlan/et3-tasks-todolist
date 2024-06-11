8456de4e2b63d4703642b919f8ddeb3118b4f0db
fouadkachlan <fouad.kachlan@et3arraf.com>
2024-06-11 12:44:01 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,17 +2,24 @@ import React from 'react';
 import ProductList from './Components/ProductList';
 import ShoppingCart from './Components/ShoppingCart';
 import UserInfo from './Components/UserInfo';
-import './App.css';
+import CustomDiv from './Components/CustomDiv';
+
 
 
 const App : React.FC = () => {
   return (
-      <div>
-        <h1>Online Shopping Cart</h1>
+      <CustomDiv alignItems= "center" justifyContent="space-between" display="block"  width="" height=""
+       style={{
+        fontFamily: "Arial, sans-serif",
+        margin: "0",
+        padding:"0",
+        boxSizing: "border-box",
+        backgroundColor: "#f7f7f7"}}>
+        <h1 style={{color: "#333"}}>Online Shopping Cart</h1>
         <UserInfo />
         <ProductList />
         <ShoppingCart />
-      </div>
+      </CustomDiv>
   );
 }
 
