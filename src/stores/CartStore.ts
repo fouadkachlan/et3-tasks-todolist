1c80a631f90228327d98ab2c4b8071233ff4d19f
fouadkachlan <fouad.kachlan@et3arraf.com>
2024-06-11 13:51:47 +0300

    Update CartStore.ts

--- src/stores/CartStore.ts
+++ src/stores/CartStore.ts
@@ -10,64 +10,59 @@ interface CartItem extends Product
 {
     quantity: number;
 }
-class CartStore
-{
-    totalitems= observable.box(0);
-    items: CartItem[] = [];
-    constructor()
-    {
+class CartStore {
+    totalItems = observable.box(0);
+    items = observable.array<CartItem>([]);
+
+    constructor() {
         makeAutoObservable(this);
     }
-    addItem(product:Product) : void
-    {
-        const existingItem = this.items.find(item => item.id === product.id)
-        if (existingItem)
-        {
+
+    addItem(product: Product): void {
+        const existingItem = this.items.find(item => item.id === product.id);
+        if (existingItem) {
             existingItem.quantity++;
-        }else{
-            this.items.push({...product, quantity:1});
+        } else {
+            this.items.push({...product, quantity: 1});
         }
         console.log("Items after adding:", this.items);
         this.updateTotalItems();
     }
-    removeItem(productId:number): void
-    {
+
+    removeItem(productId: number): void {
         const itemIndex = this.items.findIndex(item => item.id === productId);
-        if (itemIndex > -1)
-        {
-            if (this.items[itemIndex].quantity > 1)
-            {
-                this.items[itemIndex].quantity--;
-            }else{
-                this.items.splice(itemIndex,1);
+        if (itemIndex > -1) {
+            const currentItem = this.items[itemIndex];
+            if (currentItem.quantity > 1) {
+                currentItem.quantity--;
+            } else {
+                this.items.splice(itemIndex, 1);
             }
             console.log("Items after removing:", this.items);
             this.updateTotalItems();
         }
     }
+
     updateTotalItems(): void {
         const newTotalItems = this.getTotalItems();
         console.log("Updating total items to:", newTotalItems);
-        this.totalitems.set(newTotalItems);
-      }
-      getTotalItems(): number {
+        this.totalItems.set(newTotalItems);
+    }
+
+    getTotalItems(): number {
         return this.items.reduce((total, item) => total + item.quantity, 0);
-      }
-      
-        
-      get totalPrice(): number {
+    }
+
+    get totalPrice(): number {
         return this.items.reduce((total, item) => total + item.price * item.quantity, 0);
-      }
-      get renderedItems() {
+    }
+
+    get renderedItems() {
         return this.items.map(item => ({
             ...item,
             removeItem: () => this.removeItem(item.id)
         }));
-      }
+    }
 }
-
-  
-    
-    
 
 export const cartStore = new CartStore();
