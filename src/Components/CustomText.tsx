0b73cc95a6fa7e66df28ffcd8bb45c1890a159ad
fouadkachlan <fouad.kachlan@et3arraf.com>
2024-06-11 13:42:10 +0300

    Create CustomText.tsx

---
+++ src/Components/CustomText.tsx
@@ -0,0 +1,21 @@
+import React, { CSSProperties, ReactNode } from 'react'
+
+interface CustomTextProps
+{
+    children: React.ReactNode;
+    style?: React.CSSProperties;
+    fontWeight: string;
+    fontSize: string;
+}
+
+const CustomText : React.FC<CustomTextProps> = ({children , style , fontWeight , fontSize }) =>
+{
+  const basicStyles : CSSProperties = {
+    fontWeight,
+    fontSize
+  };
+  const finalizedStyles = {...basicStyles , style};
+  return <p style={finalizedStyles}>{children}</p>
+}
+
+export default CustomText
