1c13d13739668ad38f09a85d28f16bae075e9633
fouadkachlan <fouad.kachlan@et3arraf.com>
2024-06-11 12:42:21 +0300

    Update UserInfo.tsx

--- src/Components/UserInfo.tsx
+++ src/Components/UserInfo.tsx
@@ -1,14 +1,22 @@
 import React from "react"
 import { observer } from "mobx-react-lite"
 import { userStore } from "../stores/UserStore"
+import CustomDiv from "./CustomDiv";
 
 
 const UserInfo: React.FC = observer(() => {
     return (
-        <div className="user-info">
-            <h2>User Info</h2>
-            <div className="user-name"> Name: {userStore.user.name}</div>
-        </div>
+        <CustomDiv style={{
+            background: "#fff",
+            padding: "20px",
+            borderRadius: "8px",
+            boxShadow: "0 2px 5px rgba(0, 0, 0, 0.1)",
+            marginBottom: "20px"
+        }} alignItems= "center" justifyContent="space-between" display="flex"  width="" height="">
+            <h2 style={{color:"green", margin: "0"}}>User Info</h2>
+            <CustomDiv style={{}} height="" width="" display="flex" 
+            alignItems="center" justifyContent="center" >Name: {userStore.user.name} </CustomDiv>
+        </CustomDiv>
     );
 });
 export default UserInfo;
