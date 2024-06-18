69c58b1c7f4a8bab966bef7fff7098fe8b440be1
fouadkachlan <fouad.kachlan@et3arraf.com>
2024-06-18 09:52:41 +0300

    Update AboutMe.tsx

--- src/Pages/AboutMe.tsx
+++ src/Pages/AboutMe.tsx
@@ -5,7 +5,7 @@ import CustomButton from '../CustomComponents/CustomButton'
 const AboutMe = () => {
   const navigate = useNavigate();
   return (
-    <CustomDiv style={{}} width=""  height=""  display="flex"  alignItems="center"  justifyContent="center">
+    <CustomDiv style={{}} width='30%'  height='30%'  display="flex"  alignItems="center"  justifyContent="center">
         Page 2
         <CustomButton style={{marginTop:"300px" , marginRight:"50%"}} onClick={() => navigate('/dashboard')}  color="blue"
          >Return To Dashboard</CustomButton>
