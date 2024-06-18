55c1d328dd88e6ff1c52fc78918b2a483572ab06
fouadkachlan <fouad.kachlan@et3arraf.com>
2024-06-18 11:23:01 +0300

    Update UIStore.ts

--- src/Stores/UIStore.ts
+++ src/Stores/UIStore.ts
@@ -1,42 +1,33 @@
-// uiStore.ts
-import { observable, action, computed , makeObservable} from 'mobx';
+import { makeAutoObservable, runInAction } from 'mobx';
 import memoize from 'lodash.memoize';
+
 class UiStore {
-  
-   _isDrawerOpen: boolean = false;
-   _selectedLanguage : string = 'English';
-   constructor()
-   {
-    makeObservable(this,{
-      _isDrawerOpen: observable,
-      _selectedLanguage: observable,
-      toggleDrawer: action,
-      setLanguage: action,
-      isDrawerOpen: computed,
-      selectedLanguage: computed
-    });
-    this.toggleDrawer = this.toggleDrawer.bind(this);
-    //this.toggleDrawer = this.toggleDrawer.bind(this);
-    //this.setLanguage = this.setLanguage.bind(this);
-   }
-  toggleDrawer() {
+  _isDrawerOpen: boolean = false;
+  _selectedLanguage: string = 'English';
+
+  constructor() {
+    makeAutoObservable(this);
+  }
+
+  toggleDrawer = () => {
     this._isDrawerOpen = !this._isDrawerOpen;
-  }
-  setLanguage(language: string)
-  {
-    this._selectedLanguage = language;
-  }
+  };
+
+  setLanguage = (language: string) => {
+    runInAction(() => {
+      this._selectedLanguage = language;
+    })
+  };
 
   get isDrawerOpen() {
     return this._isDrawerOpen;
   }
-  get selectedLanguage()
-  {
+
+  get selectedLanguage() {
     return this._selectedLanguage;
   }
 }
 
-
-export const getUIStore = memoize(() => {return new UiStore()}, ()=>1)
-export  default getUIStore;
+export const getUIStore = memoize(() => new UiStore());
+export default getUIStore;
 export const uiStore = new UiStore();
