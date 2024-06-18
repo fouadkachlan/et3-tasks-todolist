d80eb8cd9ac9b644432cb75e7e7fc64ef7ebeca6
fouadkachlan <fouad.kachlan@et3arraf.com>
2024-06-18 09:53:51 +0300

    Update MyBrands.tsx

--- src/Pages/MyBrands.tsx
+++ src/Pages/MyBrands.tsx
@@ -7,7 +7,7 @@ import { useNavigate } from 'react-router-dom'
 const MyBrands = () => {
   const navigate = useNavigate();
   return (
-    <CustomDiv style={{marginTop:"300px" , marginRight:"50%"}} width=""  height=""  display="flex"  alignItems="center"  justifyContent="center">
+    <CustomDiv style={{marginTop:"300px" , marginRight:"50%"}} width='30%'  height='30%'  display="flex"  alignItems="center"  justifyContent="center">
         Page 4
         <CustomButton style={{marginTop:"300px" , marginRight:"50%"}} onClick={() => navigate('/dashboard')}  color="blue"
          >Return To Dashboard</CustomButton>
