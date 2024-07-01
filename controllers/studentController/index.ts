632d1c78e45ee2ba3c756e38a8926bc21a7c68c0
FouadKachlan <fouad.kachlan@et3arraf.com>
2024-07-01 14:45:00 +0300

    7th commit

--- controllers/studentController/index.ts
+++ controllers/studentController/index.ts
@@ -1,11 +1,12 @@
 import { Router } from 'express';
 import { createStudent , getAllStudents , loginStudent , registerStudent} from './studentController';
 import gatekeeper from '../../middleware/gatekeeper';
+import rateLimiter from '../../middleware/rateLimiter';
 
 const router = Router();
 
 router.post('/createStudent', gatekeeper(['admin']), createStudent);
-router.post('/students' , gatekeeper() ,getAllStudents);
+router.post('/students' , rateLimiter() ,getAllStudents);
 router.post('/register' , gatekeeper() , registerStudent);
 router.post('/login', gatekeeper() ,loginStudent );
 
--- middleware/rateLimiter.ts
+++ middleware/rateLimiter.ts
@@ -3,32 +3,49 @@ import rateLimit from "express-rate-limit";
 
 
 const rateLimitOptions = {
-    maxRequests: 100,
-    windowMS : 60 * 60 * 1000 // time window ib milliseconds for conting requests, so here we are setting 1 hour window
+    default: {
+        maxRequests: 100,
+        windowMS: 60 * 60 * 1000, // 1 hour window
+    },
+    routes : {
+        "/students" : {maxRequests : 5 , windowMS : 60 * 1000},
+        "/login" : {maxRequests : 2 , windowMS : 60 * 1000}
+    }
+};
+
+const getRateLimitOptions = (req :Request) => {
+    const routName = req.route.path || req.originalUrl;
+    return rateLimitOptions.routes["/students"] || rateLimitOptions.default; //here students for example
 }
 
+const createRateLimiter = () => {
+    return (req: Request, res: Response, next: NextFunction) => {
+        const limiterOptions = getRateLimitOptions(req);
+        const { windowMS, maxRequests } = limiterOptions;
 
+        const limiter = rateLimit({
+            windowMs: windowMS,
+            max: maxRequests,
+            message: "Too many requests, please try again later.",
+            keyGenerator: (req: Request) => {
+                const userId = (req.user && req.user.id) || 'anonymous';
+                return `${req.ip}_${userId}`;
+            }
+        });
 
-const limiter = rateLimit({
-    windowMs: rateLimitOptions.windowMS,
-    max: rateLimitOptions.maxRequests,
-    message: "Too many requests, please try again later.",
-    keyGenerator: (req: Request) => {
-        //here we are generating a unique key for rate limiting based on IP and userID
-        const userId = (req.user && req.user.id) || 'anonymous';
-        return `${req.ip}_${userId}`;
-    }
-});
-
-const rateLimiter = () => {
-    return (req: Request , res: Response , next: NextFunction) => {
-        limiter(req,res,(err : any) => {
+        limiter(req, res, (err: any) => {
             if (err) {
-                return res.status(429).json({message: "Rate limit exceeded , please try again later"})
+                return res.status(429).json({ message: "Rate limit exceeded, please try again later." });
             }
             next();
-        })
-    }
-}
+        });
+    };
+};
+
+// Middleware function to apply rate limiting
+const rateLimiter = () => {
+    const limiter = createRateLimiter();
+    return limiter;
+};
 
 export default rateLimiter;
